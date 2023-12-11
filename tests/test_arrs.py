754d2c4420d29965d13ed7b7623a40d2f8afe359
Larisa Mikhaylova <larisa.mih2004@gmail.com>
2023-12-11 15:40:33 +0300

    100 percent test coverage tests/test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,6 +4,12 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([2, 1, 4], 2, "test") == 4
+    assert arrs.get([5, 10, -1], 4, "test") == 'test'
+    assert arrs.get([1, 2, 3, 4, 5], 10, default="Not found") == "Not found"
+    assert arrs.get([1, 2, 3, 4, 5], 3) is not None
+    assert arrs.get([], 0, default="Empty") == "Empty"
+    assert arrs.get([1, 2, 3, 4, 5], 3) == int("4")
 
 
 def test_slice():
